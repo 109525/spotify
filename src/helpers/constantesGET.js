ffdcb19388a4285dce036a0550c6de565fa8d55e
407 <407@academicos.cesde.edu.co>
2022-09-20 17:41:50 -0500

    se crean las canciones

---
+++ src/helpers/constantesGET.js
@@ -0,0 +1,6 @@
+const TOKEN=" Bearer BQCHZE5u-LXkBBj-9FyYBsxLVosopsuX5RlOH1T3CtXkXcxRF2V4NH0Fj9RvPah8_gB7YHqRQZBqRphBjfDOxQEjunBLeBuVI3n3RdWgb65ysssWLNnEhuIuG9HuYr6LDJPV9QHhtXF8gjyDMX-rtzLdfDRX14PECfBRJqxw0s5clWnaE-u3fWLPJ4omFmZCca8"
+
+export const PETICION={
+    method:"GET",
+    headers:{Authorization:TOKEN}
+} 
--- src/services/servicioConsultarCanciones.js
+++ src/services/servicioConsultarCanciones.js
@@ -1,24 +1,17 @@
-//Receta para consumir apis con JS puro
+let URI="https://api.spotify.com/v1/artists/2ye2Wgw4gimLv2eAKyk1NB/top-tracks?market=US";
 
-// si yo quiero consumir un api debop saber para donde ir y a que servicio
-//debo configurar la URI
-
-const URI="https://api.spotify.com/v1/artists/2ye2Wgw4gimLv2eAKyk1NB/top-tracks?market=US"
-
-//2. configuro datos especiales o de control en el servidor
-const TOKEN=" Bearer BQCt6Pqt3Cq9KmG1oS04riyFX3PPOkwSx2I5TCv45g5yfJbgg8cYTaS-4jjEVEt5NU3aSdWDo7YhmcBfhfK1GyLt7CFEy2r1b8MRBHY8bLtx4Hmo4BvW348DXwWsyZChr5_ZiBQTYTtG4AIAb3HKC007Ld0tJf_Hia16OPK875YddcIAVMY2BBTS5AkJ-XpcvUA"
-
-//3. Configuro la peticioón 
-//NOTA: solo post y PUT configuran body
-//para js la petición es un objeto
-const PETICION={
-    method:"GET",
-    headers:{Authorization:TOKEN}
-} 
-
-//4. arranque pues mijo
-//consuma el API
-fetch(URI,PETICION)
+let buscar =document.getElementById("buscar")
+buscar.addEventListener("click",function(evento){
+    evento.preventDefault()
+    let artista=document.getElementById("artista").value 
+    console.log(artista)
+    URI=`https://api.spotify.com/v1/artists/${artista}/top-tracks?market=US`
+    const TOKEN=" Bearer BQCHZE5u-LXkBBj-9FyYBsxLVosopsuX5RlOH1T3CtXkXcxRF2V4NH0Fj9RvPah8_gB7YHqRQZBqRphBjfDOxQEjunBLeBuVI3n3RdWgb65ysssWLNnEhuIuG9HuYr6LDJPV9QHhtXF8gjyDMX-rtzLdfDRX14PECfBRJqxw0s5clWnaE-u3fWLPJ4omFmZCca8"
+    const PETICION={
+        method:"GET",
+        headers:{Authorization:TOKEN}
+    } 
+    fetch(URI,PETICION)
 .then(function(respuesta){
     return respuesta.json() //garantizo formato JSON5
 })
@@ -27,16 +20,75 @@ fetch(URI,PETICION)
     console.log(respuesta.tracks) //accedo a un atributo de la respuesta
 
     //recorrer un arreglo 
+    let fila=document.getElementById("fila")
+    fila.innerHTML=""
     respuesta.tracks.forEach(function(cancion){
-        console.log(cancion)
+       /*console.log(cancion)
         console.log(cancion.name)
         console.log(cancion.preview_url)
         console.log(cancion.album.images[0].url)
         console.log(cancion.popularity)
         console.log(cancion.album.name)
-        console.log(cancion.album.release_date)
+        console.log(cancion.album.release_date)*/
+        let imagen=document.createElement("img")
+        imagen.classList.add("h-100","w-100","img-fluid")
+        imagen.src=cancion.album.images[0].url
+
+        let nombre=document.createElement("h3")
+        nombre.classList.add("text-dark","h-50","fs-6","text-center")
+        nombre.textContent=cancion.name
+
+        let popularity=document.createElement("p")
+        popularity.classList.add("text-center")
+        popularity.textContent= textcontent="popularidad: "+ cancion.popularity
+
+
+
+        let columna=document.createElement("div")
+        columna.classList.add("col")
+
+        let tarjeta =document.createElement("div")
+        tarjeta.classList.add("card","h-100","shadow","bg-secondary")
+
+        let audio=document.createElement("audio")
+        audio.classList.add("w-100")
+        audio.setAttribute("controls","controls")
+        audio.src=cancion.preview_url
+
+        //padres e hijos
+        
+        tarjeta.appendChild(imagen)
+        tarjeta.appendChild(nombre)
+        tarjeta.appendChild(popularity)
+        tarjeta.appendChild(audio)
+        columna.appendChild(tarjeta)
+        fila.appendChild(columna)
     })
 })
 .catch(function(respuestaError){
     console.log(respuestaError)
 })
+})
+
+
+
+
+
+
+//Receta para consumir apis con JS puro
+
+// si yo quiero consumir un api debop saber para donde ir y a que servicio
+//debo configurar la URI
+
+
+
+//2. configuro datos especiales o de control en el servidor
+
+
+//3. Configuro la peticioón 
+//NOTA: solo post y PUT configuran body
+//para js la petición es un objeto
+
+
+//4. arranque pues mijo
+//consuma el API
